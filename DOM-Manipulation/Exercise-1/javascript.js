a3c75f4a35e5a064f60364a0b5a83064b17a3092
freelogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-09-17 19:21:19 -0400

    Adding a child to the container class, with  textContent too

---
+++ DOM-Manipulation/Exercise-1/javascript.js
@@ -0,0 +1,7 @@
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "Adding Text Content via Javascript File!";
+
+container.appendChild(content);
