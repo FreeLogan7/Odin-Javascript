b7da7fd24f14ae0b578f142c40673d12e425b091
freelogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-09-17 19:28:20 -0400

    adding child with style for red text

--- DOM-Manipulation/Exercise-1/javascript.js
+++ DOM-Manipulation/Exercise-1/javascript.js
@@ -1,7 +1,18 @@
 const container = document.querySelector("#container");
 
+//CLASS CONTENT
 const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "Adding Text Content via Javascript File!";
 
 container.appendChild(content);
+
+
+//CLASS RED
+const red = document.createElement("p");
+red.classList.add("red");
+red.textContent = "Hey I'm red!";
+red.style.color = "red";
+
+container.appendChild(red);
+
