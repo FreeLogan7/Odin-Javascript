2c6d8099c076e42f19335638530aa2ceb4738bb4
freelogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-09-17 19:34:20 -0400

    create blue h3

--- DOM-Manipulation/Exercise-1/javascript.js
+++ DOM-Manipulation/Exercise-1/javascript.js
@@ -16,3 +16,10 @@ red.style.color = "red";
 
 container.appendChild(red);
 
+//CLASS blue
+const blue = document.createElement("h3");
+blue.classList.add("blue");
+blue.textContent = "I'm a blue h3";
+blue.style.cssText = "color: blue;";
+
+container.appendChild(blue);
